75bea722e50ee10491eef6d0e5f0f2ff327524f5
Tanya Sahu <tanyasahutannu@gmail.com>
2025-09-16 21:39:53 +0530

    1st commit

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+     int age = 20;
+     float pi = 3.14;
+     char grade = 'A';
+     printf("hello students!!\n");
+     printf("age = %d\n",age);
+     printf("pi =%f\n",pi);
+     printf("grade = %c\n",grade);
+
+    
+    return 0;
+}
