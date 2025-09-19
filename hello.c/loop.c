ae7175c36125709920c0cf1544fdd64c2ace2867
Tanya Sahu <tanyasahutannu@gmail.com>
2025-09-19 14:04:54 +0530

    Demo

--- hello.c/loop.c
+++ hello.c/loop.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-int main(){
-    int i, n;
-    printf("enter the value of n:");
-    for(i=0;i<=n;i++){
-        printf("%d",&i);
-    }
+#include<conio.h>
+int main(){  
+    printf("my name is aradhya!!");
     return 0;
 }
+
